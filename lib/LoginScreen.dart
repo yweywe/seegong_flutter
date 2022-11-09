8ed0192b237ffaf3e362fff0972e3e0a04e0bd8b
ywe <42719337+Morrki@users.noreply.github.com>
2022-11-09 18:26:38 +0900

    기능추가 : 로그인 스크린 작성
     작업내용
     - 위젯 배치만 진행
     - 로고 이미지는 시소 기존 이미지 재활용
     - 로그인 기능 없음
    
     TODO
     - 로그인 기능 추가
     - 로그인 버튼 구글 디자인 가이드 준수

---
+++ lib/LoginScreen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Seegong-login screen',
+        home: Scaffold(
+            body: Center(
+          child: _buildBody(),
+        )));
+  }
+}
+
+Widget _buildBody() {
+  return SingleChildScrollView(
+      child: Column(
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: <Widget>[
+      Column(
+        children: <Widget>[
+          Image.asset(
+            'assets/images/glob_top_img_none.png',
+          ),
+          const Text(
+            "공유 플랫폼 시소",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+          ),
+          const Text(
+            "당신과 시흥을 잇습니다",
+            style: TextStyle(fontSize: 25),
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 250),
+            child:
+            const Text(
+              "로그인하여 시작하세요",
+              style: TextStyle(color: Colors.grey, fontSize: 18),
+            ),
+            // TODO Add Google login button
+            // TODO Follow Google's design guide
+          ),
+          ElevatedButton(
+              onPressed: () {},
+              child:
+              const Text('Google Login Button'),
+          ),
+        ],
+      )
+    ],
+  ));
+}
