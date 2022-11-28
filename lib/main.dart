81c67f4972ccc249133f054ef2f36d142d22f9ec
MaraMincho <seemaster300@naver.com>
2022-11-28 14:26:43 +0900

    기능추가: GetX 라이브러리 추가(1)
    GetX라이브러리 추가

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return GetMaterialApp( //getXMaterialApp 추가->
       title: 'Seegong',
       theme: ThemeData(
         fontFamily: 'gsans',
