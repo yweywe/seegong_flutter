a5109b18bcdf4d612f271071d3dc9a9e177b010d
ywe <42719337+Morrki@users.noreply.github.com>
2022-12-29 14:10:11 +0900

    기능추가 : url launcher 기능 이식
    작업 내용
     - url launcher 기능 추가

--- lib/screens/NavigationMenu.dart
+++ lib/screens/NavigationMenu.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:seegong_flutter/viewModel/LoginViewModel.dart';
 import 'package:seegong_flutter/screens/ReservationList.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 /// 네비게이션 메뉴 표시 전용 위젯
 ///
@@ -12,7 +13,10 @@ class NavigationMenu extends StatelessWidget {
   }) : super(key: key);
   static const customDivider = const Padding(
     padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
-    child: const Divider(height: 1, color: const Color.fromRGBO(142, 142, 142, 1), thickness: 0.5),
+    child: const Divider(
+        height: 1,
+        color: const Color.fromRGBO(142, 142, 142, 1),
+        thickness: 0.5),
   );
   @override
   Widget build(BuildContext context) {
@@ -25,11 +29,19 @@ class NavigationMenu extends StatelessWidget {
           /// 상단부 계정 헤더
           UserAccountsDrawerHeader(
             currentAccountPicture: CircleAvatar(
-              backgroundImage: NetworkImage('${currentUserController.user.value.userProfile}'),
+              backgroundImage: NetworkImage(
+                  '${currentUserController.user.value.userProfile}'),
               radius: 24,
             ),
-            accountName: Text('${currentUserController.user.value.userName}', style: TextStyle(fontSize: 17, color: Colors.white),),
-            accountEmail: Text('${currentUserController.user.value.email}', style: TextStyle(fontSize: 14, color: Color.fromRGBO(255, 255, 255, 0.4)),),
+            accountName: Text(
+              '${currentUserController.user.value.userName}',
+              style: TextStyle(fontSize: 17, color: Colors.white),
+            ),
+            accountEmail: Text(
+              '${currentUserController.user.value.email}',
+              style: TextStyle(
+                  fontSize: 14, color: Color.fromRGBO(255, 255, 255, 0.4)),
+            ),
             onDetailsPressed: () {
               print('press details');
             },
@@ -48,37 +60,72 @@ class NavigationMenu extends StatelessWidget {
                   children: [
                     ListTile(
                       // leading: Icon(Icons.notifications),
-                      title: Text('공지사항', style: TextStyle(fontSize: 14, color: Color.fromRGBO(51, 51, 51, 1), fontWeight: FontWeight.bold),),
+                      title: Text(
+                        '공지사항',
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Color.fromRGBO(51, 51, 51, 1),
+                            fontWeight: FontWeight.bold),
+                      ),
                       onTap: () {
-                        Navigator.pop(context);
+                        //Navigator.pop(context);
+                        launchUrl(
+                          Uri.parse(
+                              "https://share.siheung.go.kr/board/board_list.do?gidx=0001&pageIndex=1&key=105000"),
+                        );
                       },
                     ),
                     customDivider,
                     ListTile(
                       // leading: Icon(Icons.help),
-                      title: Text('도움말', style: TextStyle(fontSize: 14, color: Color.fromRGBO(51 , 51, 51, 1), fontWeight: FontWeight.bold),),
+                      title: Text(
+                        '도움말',
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Color.fromRGBO(51, 51, 51, 1),
+                            fontWeight: FontWeight.bold),
+                      ),
                       onTap: () {
-                        Navigator.pop(context);
+                        //Navigator.pop(context);
+                        launchUrl(
+                          Uri.parse(
+                              "https://share.siheung.go.kr/contents/contents.do?gidx=130102&key=102000"),
+                        );
                       },
                     ),
                     customDivider,
                     ListTile(
                       // leading: Icon(Icons.list),
-                      title: Text("예약내역", style: TextStyle(fontSize: 14, color: Color.fromRGBO(51, 51, 51, 1), fontWeight: FontWeight.bold),),
-                      onTap: (){
+                      title: Text(
+                        "예약내역",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Color.fromRGBO(51, 51, 51, 1),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      onTap: () {
                         Navigator.pushNamed(context, ReservationList.routename);
                       },
                     ),
                     customDivider,
                     ListTile(
                       // leading: Icon(Icons.call),
-                      title: Text("문의하기", style: TextStyle(fontSize: 14, color: Color.fromRGBO(51, 51, 51, 1), fontWeight: FontWeight.bold),),
-                      onTap: (){
-                        Navigator.pop(context);
-                      },
+                      title: Text(
+                        "문의하기",
+                        style: TextStyle(
+                            fontSize: 14,
+                            color: Color.fromRGBO(51, 51, 51, 1),
+                            fontWeight: FontWeight.bold),
+                      ),
+                      onTap: () => launchUrlString("tel:0313106043"),
+                      //onTap: () {
+                        //Navigator.pop(context);
+
+                      //},
                     ),
                   ],
                 ),
+
                 /// 로그인 버튼 시작
                 Padding(
                   padding: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 128.0),
@@ -86,13 +133,16 @@ class NavigationMenu extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.end,
                     children: [
                       TextButton(
-                        onPressed: (){
+                        onPressed: () {
                           // todo: 로그아웃 기능 추가
                           currentUserController.logout();
                         },
-                        child: Text("로그아웃", style: TextStyle(decoration: TextDecoration.underline)),
+                        child: Text("로그아웃",
+                            style: TextStyle(
+                                decoration: TextDecoration.underline)),
                         style: ButtonStyle(
-                          foregroundColor: MaterialStateProperty.all(Color.fromRGBO(153, 153, 153, 1)),
+                          foregroundColor: MaterialStateProperty.all(
+                              Color.fromRGBO(153, 153, 153, 1)),
                         ),
                       ),
                     ],
